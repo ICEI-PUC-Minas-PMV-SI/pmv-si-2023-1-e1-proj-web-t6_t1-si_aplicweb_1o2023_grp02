a8fe9c77bd007ffd86ed48044074942332cd5dad
Shiranatan <106928805+Shiranatan@users.noreply.github.com>
2023-04-19 21:46:39 -0300

    Add files via upload

---
+++ login/dump.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE login;
+USE login;
+
+CREATE TABLE `login`.`usuario` (
+  `usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(200) NOT NULL,
+  `senha` VARCHAR(32) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  `data_cadastro` DATETIME NOT NULL,
+  PRIMARY KEY (`usuario_id`));
+
+INSERT INTO `usuario` (`usuario_id`,`usuario`,`senha`, `nome`, `data_cadastro`) VALUES (1,'canalti','10f722b5984a49bce67d434464fae37e', 'Canal TI', '2019-01-11 19:42:12');
