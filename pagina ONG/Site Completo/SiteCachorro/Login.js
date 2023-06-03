c43093b9f607c2e820e5f76d167ea791c6de59d4
LEANDRO3124 <129007277+LEANDRO3124@users.noreply.github.com>
2023-06-03 19:02:06 -0300

    Add files via upload

---
+++ pagina ONG/Site Completo/SiteCachorro/Login.js
@@ -0,0 +1,8 @@
+function Login() {
+    window.location.href = "file:///C:/Users/leandro/Downloads/Cachorro/Login%20Cadastro/login.html";
+}
+
+function Regi(){
+
+    window.location.href = "file:///C:/Users/leandro/Downloads/Cachorro/Login%20Cadastro/cadastro.html"
+}
