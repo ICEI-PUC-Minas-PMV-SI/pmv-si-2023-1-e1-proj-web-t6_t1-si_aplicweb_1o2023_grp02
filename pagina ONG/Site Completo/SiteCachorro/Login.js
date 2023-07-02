e2702901ea373ba5c52dcb64dadc5110ff29c673
LEANDRO3124 <129007277+LEANDRO3124@users.noreply.github.com>
2023-07-02 18:07:16 -0300

    Delete pagina ONG/Site Completo directory

--- pagina ONG/Site Completo/SiteCachorro/Login.js
+++
@@ -1,8 +0,0 @@
-function Login() {
-    window.location.href = "file:///C:/Users/leandro/Downloads/Cachorro/Login%20Cadastro/login.html";
-}
-
-function Regi(){
-
-    window.location.href = "file:///C:/Users/leandro/Downloads/Cachorro/Login%20Cadastro/cadastro.html"
-}
