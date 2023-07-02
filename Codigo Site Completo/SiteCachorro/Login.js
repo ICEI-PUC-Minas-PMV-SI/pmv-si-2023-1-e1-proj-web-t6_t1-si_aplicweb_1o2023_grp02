e8fc6f49f11c5610edf4402da0191e5f3bc36343
LEANDRO3124 <129007277+LEANDRO3124@users.noreply.github.com>
2023-07-02 18:28:23 -0300

    Add files via upload

---
+++ Codigo Site Completo/SiteCachorro/Login.js
@@ -0,0 +1,8 @@
+function Login() {
+    window.location.href = "file:///C:/xampp/htdocs/Site%20Completo/Login%20Cadastro/Login%20Ong.php";
+}
+
+function Regi(){
+
+    window.location.href = "file:///C:/Users/leandro/Downloads/Site%20Completo/Login%20Cadastro/cadastro.php"
+}
