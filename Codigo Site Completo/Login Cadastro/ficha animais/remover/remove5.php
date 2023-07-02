105a47d16f2761c0210f8c5a52a82c037229f9c9
LEANDRO3124 <129007277+LEANDRO3124@users.noreply.github.com>
2023-07-02 18:39:14 -0300

    Add files via upload

---
+++ Codigo Site Completo/Login Cadastro/ficha animais/remover/remove5.php
@@ -0,0 +1,15 @@
+<?php 
+
+include('../conexao.php');
+
+
+$delete = "DELETE from animal where id= 5; and ong_id = 3";
+
+$remove = $conexao->query($delete) or die($conexao->error);
+
+$conexao->close();
+
+header('Location: ../ficha animais.php');
+exit;
+
+?>
