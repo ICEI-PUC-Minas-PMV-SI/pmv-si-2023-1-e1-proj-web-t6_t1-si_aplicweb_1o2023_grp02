ea4ad8d3e536703ed7fe0f7673139c519265e1db
LEANDRO3124 <129007277+LEANDRO3124@users.noreply.github.com>
2023-07-02 18:43:46 -0300

    Delete Codigo Site Completo/Login Cadastro1 directory

--- Codigo Site Completo/Login Cadastro1/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '123456');
-define('DB', 'ong');
-
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
